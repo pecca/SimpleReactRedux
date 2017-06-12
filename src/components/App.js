64e2a0b46e40fb887415808da9e54754d502033d
Pekka Mäkelä <pekka.makela@cybercom.com>
2017-06-12 15:33:59 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { PageHeader } from 'react-bootstrap'
+
+const App = () => {
+  return (
+    <div>
+      <PageHeader>
+        Simple boilerplate:<small>React and Redux</small>
+      </PageHeader>
+    </div>
+  );
+}
+
+export default App;
